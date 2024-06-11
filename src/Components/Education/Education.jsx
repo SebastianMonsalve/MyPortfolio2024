2692ee0653a0e194e58d5ea50167049ce7590edc
Sebastian Monsalve <sebastiandev16@gmail.com>
2024-06-11 16:22:06 -0500

    add: creation of the education section

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,11 +1,24 @@
-// Education.jsx
 import React from "react";
+import "./Education.css";
+import { EducationData } from "./EducationData";
 
-const Education = () => (
-  <div>
-    <h2>Education</h2>
-    <p>Details about education...</p>
-  </div>
-);
+const Education = () => {
+  return (
+    <section className="educationContainer">
+      {EducationData.map((data, index) => (
+        <div className="education" key={index}>
+          <div className="icon-edu">
+            <i className={data.icon}></i>
+          </div>
+          <div className="content-edu">
+            <h5>{data.period}</h5>
+            <h3>{data.title}</h3>
+            <p>{data.institution}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
 export default Education;
---
+++ src/Components/Education/EducationData.js
@@ -0,0 +1,32 @@
+export const EducationData = [
+  {
+    icon: "fa-solid fa-award",
+    period: "2021 - 2025",
+    title: "Informatics Engineering",
+    institution: "University institution of Envigado. IUE",
+  },
+  {
+    icon: "fa-solid fa-award",
+    period: "2018 - 2020",
+    title: "Computer network installation technician",
+    institution: "National Learning Service. SENA",
+  },
+  {
+    icon: "fa-solid fa-certificate",
+    period: "2020",
+    title: "Computer Maintenance Certification",
+    institution: "National Learning Service. SENA",
+  },
+  {
+    icon: "fa-solid fa-certificate",
+    period: "2022",
+    title: "Cisco Data Network Certification",
+    institution: "University institution of Envigado. IUE",
+  },
+  {
+    icon: "fa-solid fa-certificate",
+    period: "2024",
+    title: "Python Fundamentals Certification",
+    institution: "Skills for all with Cisco",
+  },
+];

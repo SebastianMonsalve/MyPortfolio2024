f8faf8679c4d1dfd9b4436c99ec9dfe4750e740d
Sebastian Monsalve <sebastiandev16@gmail.com>
2024-06-11 16:06:04 -0500

    add: creation of the about me section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,9 +5,29 @@ import { AboutData } from "./AboutData";
 const About = () => {
   return (
     <section className="aboutContainer">
-      <div className="about-box">
-        <div className="content-box"></div>
-      </div>
+      {AboutData.map((data, index) => (
+        <div className="about-box" key={index}>
+          <div className="content-box">
+            <div className="image-profile-box">
+              <img src="/Profile.jpg" alt="image profile" draggable="false" />{" "}
+            </div>
+            <div className="content-info-box">
+              <h2>Sebastian Monsalve</h2>
+              <p>{data.content}</p>
+            </div>
+          </div>
+          <div className="container-box">
+            <div className="image-box">
+              <img src={data.image} alt="post image" draggable="false" />
+            </div>
+          </div>
+          <div className="options-box">
+            <i className="bx bx-like" title="Like" />
+            <i className="bx bx-dislike" title="Dislike" />
+            <i className="bx bx-comment" title="Comment" />
+          </div>
+        </div>
+      ))}
     </section>
   );
 };
